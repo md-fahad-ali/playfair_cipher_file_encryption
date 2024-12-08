8fedf0ef089032c5dcd190aec3ea9d14755db772
Fahad Ali <141234330+md-fahad-ali@users.noreply.github.com>
2024-12-09 02:35:18 +0600

    Playfair cipher :tada:

--- frontend/pages/decrypt/index.js
+++ frontend/pages/decrypt/index.js
@@ -46,7 +46,7 @@ export default function Decrypt() {
 
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/decryptfile`,
+        `/api/decryptfile`,
         {
           method: "POST",
           body: formData,
