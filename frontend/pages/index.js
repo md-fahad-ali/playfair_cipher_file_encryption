6ca10fa3c1ed6e839567312d30aa87fcb8e30543
Fahad Ali <141234330+md-fahad-ali@users.noreply.github.com>
2024-12-09 02:31:31 +0600

    Playfair cipher :tada:

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
     formData.append("key", keyword);
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/upload`,
+        `/api/upload`,
         {
           method: "POST",
           body: formData,
